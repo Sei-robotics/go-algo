551923f7bd6a76f3d4e865cdaa37e0e559ecc84c
R Nanamura <rnanamura4@sei-robotics.com>
2022-05-15 20:45:15 -0400

    first implementation

---
+++ pkg/sort/sort.go
@@ -0,0 +1,135 @@
+package sort
+
+import (
+	"errors"
+	"fmt"
+)
+
+type SortableCollection interface {
+	Len() int
+
+	Less(this int, that int) bool
+
+	At(i int)
+
+	Copy(start int, end int) SortableCollection
+}
+
+func SimpleInsertionSortInt(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		val := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > val {
+			arr[j+1] = arr[j]
+			j -= 1
+		}
+		arr[j+1] = val
+	}
+}
+
+func RecursiveInsertionSortInt(arr []int, pos int) error {
+	if pos > len(arr) {
+		return fmt.Errorf("passed size %d greater than array length %d", pos, len(arr))
+	}
+
+	if pos == 1 {
+		return nil
+	}
+	if err := RecursiveInsertionSortInt(arr, pos-1); err != nil {
+		return err
+	}
+	i := pos - 1
+	val := arr[i]
+	j := i - 1
+	for j >= 0 && arr[j] > val {
+		arr[j+1] = arr[j]
+		j -= 1
+	}
+	arr[j+1] = val
+	return nil
+}
+
+// merges two sorted int slices
+// merge sorted arrays:
+// {arr[start], ..., arr[middle-1]} and
+// {arr[middle], ..., arr[end-1]}
+func MergeSortedInt(arr []int, start int, middle int, end int) error {
+	if middle < start || end < middle {
+		return errors.New("wrong indices")
+	}
+
+	len1 := middle - start
+	len2 := end - middle
+
+	left := make([]int, len1)
+	right := make([]int, len2)
+
+	for i := 0; i < len1; i++ {
+		left[i] = arr[start+i]
+	}
+	for i := 0; i < len2; i++ {
+		right[i] = arr[middle+i]
+	}
+	i := 0
+	j := 0
+	for k := start; k < end; k++ {
+		if i >= len1 {
+			arr[k] = right[j]
+			j++
+			continue
+		}
+		if j >= len2 {
+			arr[k] = left[i]
+			i++
+			continue
+		}
+		if left[i] < right[j] {
+			arr[k] = left[i]
+			i++
+		} else {
+			arr[k] = right[j]
+			j++
+		}
+	}
+	return nil
+}
+
+func MergeSortInt(arr []int, start int, end int) error {
+	if start+1 >= end {
+		return nil
+	}
+	var middle int = (end + start) / 2
+	var err error
+	err = MergeSortInt(arr, start, middle)
+	if err != nil {
+		return err
+	}
+	err = MergeSortInt(arr, middle, end)
+	if err != nil {
+		return err
+	}
+	MergeSortedInt(arr, start, middle, end)
+	return nil
+}
+
+func MergeWithInsertionSortInt(arr []int, start int, end int, threshold int) error {
+	if start+1 >= end {
+		return nil
+	}
+	if end-start <= threshold {
+		SimpleInsertionSortInt(arr[start:end])
+		return nil
+	}
+	var middle int = (end + start) / 2
+	var err error
+	err = MergeWithInsertionSortInt(arr, start, middle, threshold)
+	if err != nil {
+		return err
+	}
+	err = MergeWithInsertionSortInt(arr, middle, end, threshold)
+	if err != nil {
+		return err
+	}
+	MergeSortedInt(arr, start, middle, end)
+	return nil
+}
---
+++ pkg/sort/sort_test.go
@@ -0,0 +1,106 @@
+package sort
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+import "gotest.tools/v3/assert"
+
+func Test_InsertSort(t *testing.T) {
+
+	arr := []int{5, 2, 4, 6, 1, 3}
+	sortedArr := []int{1, 2, 3, 4, 5, 6}
+	SimpleInsertionSortInt(arr)
+	checkIntArrays(t, arr, sortedArr)
+
+}
+
+func Test_RecursiveInsertSort(t *testing.T) {
+
+	arr := []int{5, 2, 4, 6, 1, 3}
+	sortedArr := []int{1, 2, 3, 4, 5, 6}
+	err := RecursiveInsertionSortInt(arr, len(arr))
+	if err != nil {
+		t.Errorf("returned error %v", err.Error())
+	}
+	checkIntArrays(t, arr, sortedArr)
+
+}
+
+func Test_MergeSortedIntSimple(t *testing.T) {
+
+	arr := []int{0, 4, 6, 7, 1, 5, 9}
+	sortedArr := []int{0, 1, 4, 5, 6, 7, 9}
+	err := MergeSortedInt(arr, 0, 4, 7)
+	assert.NilError(t, err)
+	checkIntArrays(t, arr, sortedArr)
+}
+
+func checkIntArrays(t *testing.T, arr []int, sortedArr []int) {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != sortedArr[i] {
+			t.Errorf("incorrect value at index %d, expected %d, got %d",
+				i, sortedArr[i], arr[i])
+		}
+	}
+}
+
+func Test_MergeSortedIntOneItemAlreadySorted(t *testing.T) {
+
+	arr := []int{0, 4, 8}
+	sortedArr := []int{0, 4, 8}
+	err := MergeSortedInt(arr, 0, 1, 3)
+	assert.NilError(t, err)
+	checkIntArrays(t, arr, sortedArr)
+}
+
+func Test_MergeSortedIntOneItemToBeSorted(t *testing.T) {
+
+	arr := []int{0, 4, 2}
+	sortedArr := []int{0, 2, 4}
+	err := MergeSortedInt(arr, 0, 2, 3)
+	assert.NilError(t, err)
+	checkIntArrays(t, arr, sortedArr)
+}
+
+func Test_MergeSortIntSimple(t *testing.T) {
+
+	arr := []int{0, 4, 6, 7, 1, 5, 9}
+	sortedArr := []int{0, 1, 4, 5, 6, 7, 9}
+	err := MergeSortInt(arr, 0, 6)
+	assert.NilError(t, err)
+	checkIntArrays(t, arr, sortedArr)
+}
+
+func Test_MergeSortIntBookSample(t *testing.T) {
+
+	arr := []int{3, 41, 52, 26, 38, 57, 9, 49}
+	sortedArr := []int{3, 9, 26, 38, 41, 49, 52, 57}
+	err := MergeSortInt(arr, 0, len(arr))
+	assert.NilError(t, err)
+	checkIntArrays(t, arr, sortedArr)
+}
+
+func Test_MergeWithInsertionSortIntBookSample(t *testing.T) {
+
+	arr := []int{3, 41, 52, 26, 38, 57, 9, 49}
+	sortedArr := []int{3, 9, 26, 38, 41, 49, 52, 57}
+	err := MergeWithInsertionSortInt(arr, 0, len(arr), 4)
+	assert.NilError(t, err)
+	checkIntArrays(t, arr, sortedArr)
+}
+
+func BenchmarkMergeWithInsertionSortInt(b *testing.B) {
+	var arr []int
+	maxThreshold := 5
+	for i := 0; i < b.N; i++ {
+		arr = []int{3, 41, 52, 26, 38, 57, 9, 49}
+		threshold := i % maxThreshold
+		err := MergeWithInsertionSortInt(arr, 0, len(arr), threshold)
+		if err != nil {
+			log.Fatal(fmt.Sprintf("Error in benchmark for threshold %d", threshold))
+		}
+	}
+
+}
